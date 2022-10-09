e477385c0494baa780f7619f27eee7c6e36b4a58
Fahd Work <fhd3775@gmail.com>
2022-10-09 13:50:17 +0300

    first commit

---
+++ src/Section52Sorter.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class Section52Sorter{
+    static void sort(int[] arr){
+        int n = arr.length;
+ 
+        // One by one move boundary of unsorted subarray
+        for (int i = 0; i < n-1; i++)
+        {
+            // Find the minimum element in unsorted array
+            int min_idx = i;
+            for (int j = i+1; j < n; j++)
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+ 
+            // Swap the found minimum element with the first
+            // element
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {2,1,5,4};
+
+        System.out.println("Hello World");
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+        System.out.println("im sultan");
+        
+        
+    }
+}
